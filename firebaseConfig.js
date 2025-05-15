7d5a988698d2b2c517a370ee2190ba8f5af29213
Sanchai Saiyot <sanchai5651@gmail.com>
2025-05-15 18:26:31 +0700

    fix: decode FIREBASE_PRIVATE_KEY_BASE64 for Firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,14 +1,11 @@
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
-const privateKeyBase64 = process.env.FIREBASE_PRIVATE_KEY_BASE64;
-const privateKey = Buffer.from(privateKeyBase64, 'base64').toString('utf-8');
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64, "base64").toString("utf-8")
+);
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: privateKey,
-  }),
+  credential: admin.credential.cert(serviceAccount),
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 });
 
