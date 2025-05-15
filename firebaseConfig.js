45f5071db36d8ad8cf7a06e5b3c8a5a2311550eb
Sanchai Saiyot <sanchai5651@gmail.com>
2025-05-15 18:06:18 +0700

    fix privateKey newline for Firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,13 @@
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
+
+const privateKeyBase64 = process.env.FIREBASE_PRIVATE_KEY_BASE64;
+const privateKey = Buffer.from(privateKeyBase64, 'base64').toString('utf-8');
 
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    privateKey: privateKey,
   }),
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 });
