ba0daaf94ac7566a498171de77d610c1c1f043eb
Sanchai Saiyot <sanchai5651@gmail.com>
2025-05-16 14:53:51 +0700

    Add storageBucket to Firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,15 @@
 const admin = require("firebase-admin");
 
-// ✅ ใช้ process.env พร้อมแปลง \\n เป็น newline จริง สำหรับ privateKey
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   }),
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // ✅ เพิ่มบรรทัดนี้ มั่วไปเรื่อย
 });
 
-module.exports = admin;
+const db = admin.firestore();
+const bucket = admin.storage().bucket();
+
+module.exports = { admin, db, bucket };
